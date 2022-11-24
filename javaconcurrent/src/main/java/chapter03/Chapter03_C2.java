6cb515d6c1306b3465126db43c3572caf79af829
quanhangbo <2409711401@qq.com>
2022-11-24 22:23:52 +0800

    join

---
+++ javaconcurrent/src/main/java/chapter03/Chapter03_C2.java
@@ -0,0 +1,31 @@
+package chapter03;
+
+/**
+ * @author quanhangbo
+ * @date 2022/11/24 22:18
+ */
+public class Chapter03_C2 extends Thread {
+	// join() 作用是等待线程对象销毁
+	
+	@Override
+	public void run() {
+		try {
+			int secondValue = (int) (Math.random() * 10000);
+			System.out.println(secondValue);
+			Thread.sleep(secondValue);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+
+class Chapter03_C2_01 {
+	
+	public static void main(String[] args) {
+		Chapter03_C2 chapter03_c2 = new Chapter03_C2();
+		chapter03_c2.start();
+		// 这里并不知道子线程休息了多长时间? Thread.sleep(?)
+		System.out.println("我想要执行在main执行完之前，执行run");
+	}
+}
