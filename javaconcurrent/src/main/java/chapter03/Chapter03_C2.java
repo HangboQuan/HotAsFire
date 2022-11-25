2f1bb35ed6b89313c7da8bfdd5b4a4352358dac4
quanhangbo <2409711401@qq.com>
2022-11-25 21:31:32 +0800

    join

--- javaconcurrent/src/main/java/chapter03/Chapter03_C2.java
+++ javaconcurrent/src/main/java/chapter03/Chapter03_C2.java
@@ -21,11 +21,37 @@ public class Chapter03_C2 extends Thread {
 }
 
 class Chapter03_C2_01 {
-	
+
+	/**
+	 * 我想要执行在main执行完之前，执行run
+	 * 8959
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Chapter03_C2 chapter03_c2 = new Chapter03_C2();
 		chapter03_c2.start();
 		// 这里并不知道子线程休息了多长时间? Thread.sleep(?)
 		System.out.println("我想要执行在main执行完之前，执行run");
+	}
+}
+
+class Chapter03_C2_02 {
+	
+	public static void main(String[] args) {
+		
+		/**
+		 * 6764
+		 * 等子线程运行完，main线程结束
+		 */
+		try {
+			Chapter03_C2 chapter03_c2 = new Chapter03_C2();
+			chapter03_c2.start();
+			
+			chapter03_c2.join();
+			System.out.println("等子线程运行完，main线程结束");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
