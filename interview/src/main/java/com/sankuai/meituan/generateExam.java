379822d7a70e8e2c24bbc1414a6d7bb507ebf34e
quanhangbo <2409711401@qq.com>
2023-12-04 00:26:10 +0800

    增加权重概率出题

--- interview/src/main/java/com/sankuai/meituan/generateExam.java
+++ interview/src/main/java/com/sankuai/meituan/generateExam.java
@@ -13,10 +13,7 @@ import java.util.*;
  */
 public class generateExam {
 
-    // 通过接入redis的zset来实现
-    public static void main(String[] args) throws Exception {
-        // 随机生成30-50面试 + 1~2到算法题
-
+    public static void text1() throws Exception {
         ClassPathResource resource = new ClassPathResource("/excellent/question.txt");
         InputStream inputStream = resource.getInputStream();
         List<String> questions = IOUtils.readLines(inputStream, "utf-8");
@@ -46,5 +43,92 @@ public class generateExam {
         for (String s : res) {
             System.out.println(s);
         }
+    }
+
+    public static void text2() throws Exception {
+        ClassPathResource resource = new ClassPathResource("/excellent/question.txt");
+        InputStream inputStream = resource.getInputStream();
+        List<String> questions = IOUtils.readLines(inputStream, "utf-8");
+        List<String> questionsRes = new ArrayList<String>();
+        List<Integer> questionsScore = new ArrayList<Integer>();
+
+        int qCount = 0;
+        for (String str : questions) {
+            if (qCount == 0) {
+                qCount ++;
+                continue;
+            }
+            String[] s = str.split("\\t");
+            questionsRes.add(s[0]);
+            questionsScore.add(Integer.valueOf(s[1]));
+
+        }
+
+        List<Integer> questionsWeight = questionsScore;
+
+
+
+        ClassPathResource resource1 = new ClassPathResource("/excellent/leetcode.txt");
+        InputStream inputStream1 = resource1.getInputStream();
+        List<String> algorithms = IOUtils.readLines(inputStream1, "utf-8");
+        List<String> algorithmsRes = new ArrayList<String>();
+        List<Integer> algorithmScore = new ArrayList<Integer>();
+
+        int sCount = 0;
+        for (String str : algorithms) {
+            if (sCount == 0) {
+                sCount ++;
+                continue;
+            }
+            String[] s = str.split("\\t");
+            algorithmsRes.add(s[0]);
+            algorithmScore.add(Integer.valueOf(s[1]));
+        }
+
+        List<Integer> algorithmWeight = algorithmScore;
+
+
+        List<String> res = new ArrayList<>();
+
+        List<String> selectedQuestions = weightedRandomChoice(questionsRes, questionsWeight, 50, 80);
+        List<String> selectedAlgorithms = weightedRandomChoice(algorithmsRes, algorithmWeight, 2, 3);
+        res.addAll(selectedQuestions);
+        res.addAll(selectedAlgorithms);
+
+        for (String s : res) {
+            System.out.println(s);
+        }
+    }
+
+
+    // 通过接入redis的zset来实现
+    public static void main(String[] args) throws Exception {
+        // 随机生成30-50面试 + 1~2到算法题
+//        text1();
+        text2();
+
+
+
+    }
+
+    public static List<String> weightedRandomChoice(List<String> keys, List<Integer> weights, int min, int max) {
+        int totalWeight = weights.stream().mapToInt(Integer::intValue).sum();
+        Random random = new Random();
+        List<String> chosenKeys = new ArrayList<>();
+
+        while (chosenKeys.size() < min || chosenKeys.size() > max) {
+            double randomValue = random.nextDouble() * totalWeight;
+            double cumulativeWeight = 0;
+
+            for (int i = 0; i < keys.size(); i++) {
+                cumulativeWeight += weights.get(i);
+                if (cumulativeWeight >= randomValue) {
+                    chosenKeys.add(keys.get(i));
+                    break;
+                }
+            }
+        }
+
+        return chosenKeys;
     }
 }
