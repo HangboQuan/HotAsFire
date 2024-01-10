42a4f5520f45f4014eaaa9f8362495e7dde5f3e0
quanhangbo <2409711401@qq.com>
2024-01-10 23:13:50 +0800

    fail-fast

--- javabase/src/main/java/com/alibaba/javabase/base/FailFast.java
+++ javabase/src/main/java/com/alibaba/javabase/base/FailFast.java
@@ -1,7 +1,6 @@
 package com.alibaba.javabase.base;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author quanhangbo
@@ -25,5 +24,45 @@ public class FailFast {
         for (Map.Entry<Integer, Integer> m : map.entrySet()) {
             System.out.println(m.getKey() + " " + m.getValue());
         }
+
+        Iterator iterator = map.entrySet().iterator();
+        Map.Entry entry;
+
+        while (iterator.hasNext()) {
+            entry = (Map.Entry) iterator.next();
+            if ((Integer)entry.getKey() % 5 == 0) {
+                map.remove(entry.getKey());
+            }
+        }
+
+
+
+        // 为什么iterator迭代器 能够避免fail-fast
+        Iterator<Map.Entry<Integer, Integer>> iterator1 = map.entrySet().iterator();
+        while (iterator1.hasNext()) {
+            Map.Entry<Integer, Integer> entry1 = iterator1.next();
+            if (entry1.getKey() % 5 == 0) {
+                iterator1.remove();
+            }
+        }
+
+        Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Integer, Integer> e = it.next();
+            System.out.println(e.getKey() + " " + e.getValue());
+        }
+
+
+        List<String> ans = new ArrayList<String>() {
+                {
+                        add("abc");
+                        add("alibaba");
+                        add("pdd");
+                        add("bytedance");
+                }
+
+        };
+
+
     }
 }
