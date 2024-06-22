a77479ea54759d9e441ec6beb5b379867123b2bd
quanhangbo <2409711401@qq.com>
2024-06-22 22:33:01 +0800

    TO be continue

---
+++ leetcode/src/main/java/com/alibaba/weekly/W392/leetcode3185.java
@@ -0,0 +1,17 @@
+package com.alibaba.weekly.W392;
+
+/**
+ * @author quanhangbo
+ * @date 2024-06-22 22:30
+ */
+public class leetcode3185 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static long countCompleteDayPairs(int[] hours) {
+        
+
+    }
+}
