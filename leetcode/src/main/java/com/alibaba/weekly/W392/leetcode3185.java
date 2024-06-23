5726ab7bdc935572e6720a00df121303c603024e
quanhangbo <2409711401@qq.com>
2024-06-23 20:59:53 +0800

    TO be continue

--- leetcode/src/main/java/com/alibaba/weekly/W392/leetcode3185.java
+++ leetcode/src/main/java/com/alibaba/weekly/W392/leetcode3185.java
@@ -7,11 +7,16 @@ package com.alibaba.weekly.W392;
 public class leetcode3185 {
 
     public static void main(String[] args) {
-
+        System.out.println(countCompleteDayPairs(new int[]{12, 12, 30, 24, 24}));
     }
 
     public static long countCompleteDayPairs(int[] hours) {
-        
-
+        long ans = 0;
+        int[] cnt = new int[24];
+        for (int h : hours) {
+            ans += cnt[(24 - h % 24) % 24];
+            cnt[h % 24] ++;
+        }
+        return ans;
     }
 }
