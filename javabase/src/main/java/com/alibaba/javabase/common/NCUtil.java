7658ef683f9ed11fde0fceae347ee739abed642c
quanhangbo <2409711401@qq.com>
2025-05-29 20:29:48 +0800

    commit

--- javabase/src/main/java/com/alibaba/javabase/common/NCUtil.java
+++ javabase/src/main/java/com/alibaba/javabase/common/NCUtil.java
@@ -1,8 +1,17 @@
 package com.alibaba.javabase.common;
+import java.util.function.Supplier;
 
 /**
  * @author quanhangbo
  * @date 2025-05-17 23:18
  */
 public class NCUtil<T> {
+
+    public static <T> T ofNullable(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
