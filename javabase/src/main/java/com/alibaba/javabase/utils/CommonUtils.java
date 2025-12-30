a5d260e58aae8d836d0155b4ba271f2393fb768e
quanhangbo <2409711401@qq.com>
2025-12-30 10:26:51 +0800

    commit

---
+++ javabase/src/main/java/com/alibaba/javabase/utils/CommonUtils.java
@@ -0,0 +1,48 @@
+package com.alibaba.javabase.utils;
+
+import com.huaban.analysis.jieba.JiebaSegmenter;
+import com.huaban.analysis.jieba.SegToken;
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonUtils {
+
+    public static List<String> cutWordByJieba(String query) {
+        List<String> ans = new ArrayList<>();
+        JiebaSegmenter segmenter = new JiebaSegmenter();
+        List<SegToken> result = segmenter.process(query, JiebaSegmenter.SegMode.INDEX);
+        for (SegToken token : result) {
+            ans.add(token.word);
+        }
+        return ans;
+    }
+
+    public static List<String> cutWordByIKAnalyzer(String query) {
+        try {
+            long start = System.currentTimeMillis();
+            List<String> ans = new ArrayList<>();
+            StringReader reader = new StringReader(query);
+            IKSegmenter ikSegmenter = new IKSegmenter(reader, true);
+            Lexeme lexeme;
+            while ((lexeme = ikSegmenter.next()) != null) {
+                ans.add(lexeme.getLexemeText());
+            }
+            return ans;
+        } catch (Exception e) {
+            System.out.println("ik分词失败");
+            return new ArrayList<>();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(cutWordByJieba("大日本帝国于1937年7月7日，全面入侵中华民国"));
+        System.out.println(cutWordByIKAnalyzer("Dennis Ritchie is an American computer scientist"));
+        System.out.println(cutWordByIKAnalyzer("丹尼斯里奇是一位美国计算机科学家"));
+        System.out.println(cutWordByJieba("丹尼斯里奇是一位美国计算机科学家"));
+    }
+
+}
